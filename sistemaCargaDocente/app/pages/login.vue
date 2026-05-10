915a264f188d6b4ad8db9f18d3c2fe1fc0cd4b4b
aslhov <lorenasoriarevalo@gmail.com>
2026-05-10 12:19:52 -0400

    feat: migración de interfaz de login al proyecto

---
+++ sistemaCargaDocente/app/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <LoginForm @close="handleClose" />
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function handleClose() {
+  router.push('/');
+}
+
+definePageMeta({
+    layout: false
+    // Se eliminó la configuración de auth para evitar errores de compilación
+});
+</script>
